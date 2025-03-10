a5606ee443d34796117da5c4caafca1dc6cda51f
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-10 10:57:19 +0000

    downloaded the api the correct way

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use Illuminate\Routing\Route;
-
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
